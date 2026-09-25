718087a4b28ef16b4907f079af9eab2898219336
aslanka <49008222+aslanka@users.noreply.github.com>
2026-09-24 22:26:44 -0400

    Add Supabase client

---
+++ src/lib/supabase.ts
@@ -0,0 +1,23 @@
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AppState, Platform } from 'react-native';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://rephkhyncgpdlxrqflut.supabase.co';
+const supabasePublishableKey = 'sb_publishable_cnW7ddIZtA88Bkf7wYmiqg_WHLCtT4B';
+
+export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
+
+if (Platform.OS !== 'web') {
+  AppState.addEventListener('change', (state) => {
+    if (state === 'active') supabase.auth.startAutoRefresh();
+    else supabase.auth.stopAutoRefresh();
+  });
+}
